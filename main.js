140e3afbf08090b35244121bd647fbc5c5603507
cina <memaralicina@gmail.com>
2017-06-04 18:26:36 -0400

    working on adjusting timer, finished stopstart and reset functions

--- main.js
+++ main.js
@@ -1,17 +1,74 @@
 angular.module('tomatoApp', []);
 
-angular.module('tomatoApp').controller('mainCtrl', function ()
+angular.module('tomatoApp').controller('mainCtrl', ['$interval', function ($interval)
 {
   var vm = this;
-
+  vm.timerboolean = false;
+  vm.intervalpromise;
   vm.timernumber = 1500;
+  vm.adjuststate = false;
   // vm.timernumber = 1400;
-  vm.play = function()
+
+  // vm.theinterval = function()
+  // {
+  //   vm.timernumber-=1;
+  // };
+
+  //if you click it again, it stops, how do you check if function is running?
+
+  vm.startstop = function()
   {
-    vm.timernumber-1;
+
+    var theinterval = function()
+    {
+      vm.timernumber-= 1;
+    };
+
+
+    if(vm.timerboolean == false)
+    {
+      vm.intervalpromise = $interval(theinterval, 1000);
+      vm.timerboolean = true;
+    }
+    else
+    {
+
+
+      $interval.cancel(vm.intervalpromise);
+      vm.timerboolean = false;
+
+    }
+    // if(vm.timerboolean)
+    // {
+    //   $interval.cancel(vm.intervalpromise);
+    //   vm.timerboolean = false;
+    // }
+    // else
+    // {
+    //   vm.intervalpromise = $interval(theinterval, 1000);
+    //   vm.timerboolean = true;
+    //
+    // }
 
   };
-});
+
+
+
+  vm.reset = function()
+  {
+    vm.timernumber = 1500;
+  };
+
+
+  vm.adjust = function()
+  {
+    if(vm.adjuststate == false)
+    {
+      vm.adjuststate = true;
+
+    }
+  };
+}]);
 
 
 angular.module('tomatoApp').filter('minsecFormat', function()
@@ -24,12 +81,17 @@ angular.module('tomatoApp').filter('minsecFormat', function()
 
     if (sec == 0)
     {
-      return min + " : 00";
+      return min + ":00";
 
+    }
+
+    else if (sec > 1 && sec < 10)
+    {
+      return min + ":0" + sec;
     }
     else
     {
-      return min + " : " + sec;
+      return min + ":" + sec;
 
     }
   }
